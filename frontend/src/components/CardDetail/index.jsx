18b6e52603e68d215b6f032948075509bc9a0c54
neko <nekospell.mkt@gmail.com>
2024-08-13 16:33:19 +0800

    starter img part

--- frontend/src/components/CardDetail/index.jsx
+++ frontend/src/components/CardDetail/index.jsx
@@ -9,125 +9,167 @@ import Wind from "@assets/Wind.png";
 import Thunder from "@assets/Thunder.png";
 import { addCommaInNumber } from "@/interface.js";
 
-export default function CardDetail({item}) {
-    
-    const element = (item) => {
-        if (item?.Element === "Wind") {
-            return Wind
-        } else if (item?.Element === "Water") {
-            return Water
-        } else if (item?.Element === "Earth") {
-            return Earth
-        } else if (item?.Element === "Fire") {
-            return Fire
-        } else {
-            return Thunder
-        }
-    }
-    
-    return (
-        <div
-            style={{
-                backgroundImage: `url(${element(item)})`,
-                backgroundSize: "cover",
-                backgroundPosition: 'center',
-                width: "185px",
-                height: "230px",
-                position: "relative",
-            }}
-        >
-            <div style={{
-                textShadow: "2px 0 0 black, -2px 0 0 black, 0 2px 0 black, 0 -2px 0 black, 1px 1px black, -1px -1px black, 1px -1px black, -1px 1px black",
-                position: 'absolute',
-                top: '17px',
-                left: '19px',
-                fontFamily: 'BIG SHOT',
-                color: "yellow",
-                fontSize: "9px",
-            }}>
-                {"Lv " + item?.Level}
-            </div>
-            <div style={{
-                textShadow: "2px 0 0 black, -2px 0 0 black, 0 2px 0 black, 0 -2px 0 black, 1px 1px black, -1px -1px black, 1px -1px black, -1px 1px black",
-                position: 'absolute',
-                top: '17px',
-                right: '19px',
-                fontFamily: 'BIG SHOT',
-                color: "white",
-                fontSize: "9px",
-            }}>
-                {"Fade=" + item?.Fade}
-            </div>
-            <div style={{
-                textShadow: "2px 0 0 black, -2px 0 0 black, 0 2px 0 black, 0 -2px 0 black, 1px 1px black, -1px -1px black, 1px -1px black, -1px 1px black",
-                position: 'absolute',
-                top: '152px',
-                left: '50%',
-                transform: 'translate(-50%, -50%)',
-                fontFamily: 'BIG SHOT',
-                color: "white",
-                fontSize: "10px",
-            }}>
-                {item?.Mana}
-            </div>
-            <div style={{
-                textShadow: "2px 0 0 black, -2px 0 0 black, 0 2px 0 black, 0 -2px 0 black, 1px 1px black, -1px -1px black, 1px -1px black, -1px 1px black",
-                position: 'absolute',
-                top: '170px',
-                left: '21px',
-                fontFamily: 'BIG SHOT',
-                color: "white",
-                fontSize: "10px",
-            }}>
-                <div style={{flexDirection: "row", display: "flex", alignItems: "center"}}>
-                    <img src={SPI} width={18} style={{marginRight: "4px"}} alt={""}/>
-                    <span>{item?.SPI}< /span>
-                </div>
-            </div>
-            <div style={{
-                textShadow: "2px 0 0 black, -2px 0 0 black, 0 2px 0 black, 0 -2px 0 black, 1px 1px black, -1px -1px black, 1px -1px black, -1px 1px black",
-                position: 'absolute',
-                top: '190px',
-                left: '21px',
-                fontFamily: 'BIG SHOT',
-                color: "white",
-                fontSize: "10px",
-            }}>
-                <div style={{flexDirection: "row", display: "flex", alignItems: "center"}}>
-                    <img src={DEF} width={18} style={{marginRight: "4px"}} alt={""}/>
-                    <span>{item?.DEF}< /span>
-                </div>
-            </div>
-            <div style={{
-                textShadow: "2px 0 0 black, -2px 0 0 black, 0 2px 0 black, 0 -2px 0 black, 1px 1px black, -1px -1px black, 1px -1px black, -1px 1px black",
-                position: 'absolute',
-                top: '170px',
-                left: '100px',
-                fontFamily: 'BIG SHOT',
-                color: "white",
-                fontSize: "10px",
-            }}>
-                <div style={{flexDirection: "row", display: "flex", alignItems: "center"}}>
-                    <img src={ATK} width={18} style={{marginRight: "4px"}} alt={""}/>
-                    <span>{item?.ATK}< /span>
-                </div>
-            </div>
-            <div style={{
-                textShadow: "2px 0 0 black, -2px 0 0 black, 0 2px 0 black, 0 -2px 0 black, 1px 1px black, -1px -1px black, 1px -1px black, -1px 1px black",
-                position: 'absolute',
-                top: '190px',
-                left: '100px',
-                fontFamily: 'BIG SHOT',
-                color: "white",
-                fontSize: "10px",
-            }}>
-                <div style={{flexDirection: "row", display: "flex", alignItems: "center"}}>
-                    <img src={SPD} width={18} style={{marginRight: "4px"}} alt={""}/>
-                    <span>{item?.SPD}< /span>
-                </div>
-            </div>
-            }}
-        </div>
-    );
-}
+export default function CardDetail({ item }) {
+	const element = (item) => {
+		if (item?.Element === "Wind") {
+			return Wind;
+		} else if (item?.Element === "Water") {
+			return Water;
+		} else if (item?.Element === "Earth") {
+			return Earth;
+		} else if (item?.Element === "Fire") {
+			return Fire;
+		} else {
+			return Thunder;
+		}
+	};
 
+	return (
+		<div
+			style={{
+				backgroundImage: `url(${element(item)})`,
+				backgroundSize: "cover",
+				backgroundPosition: "center",
+				width: "185px",
+				height: "230px",
+				position: "relative",
+			}}
+		>
+			<div
+				style={{
+					textShadow:
+						"2px 0 0 black, -2px 0 0 black, 0 2px 0 black, 0 -2px 0 black, 1px 1px black, -1px -1px black, 1px -1px black, -1px 1px black",
+					position: "absolute",
+					top: "17px",
+					left: "19px",
+					fontFamily: "BIG SHOT",
+					color: "yellow",
+					fontSize: "9px",
+				}}
+			>
+				{"Lv " + item?.Level}
+			</div>
+			<div
+				style={{
+					textShadow:
+						"2px 0 0 black, -2px 0 0 black, 0 2px 0 black, 0 -2px 0 black, 1px 1px black, -1px -1px black, 1px -1px black, -1px 1px black",
+					position: "absolute",
+					top: "17px",
+					right: "19px",
+					fontFamily: "BIG SHOT",
+					color: "white",
+					fontSize: "9px",
+				}}
+			>
+				{"Fade=" + item?.Fade}
+			</div>
+			<div
+				style={{
+					textShadow:
+						"2px 0 0 black, -2px 0 0 black, 0 2px 0 black, 0 -2px 0 black, 1px 1px black, -1px -1px black, 1px -1px black, -1px 1px black",
+					position: "absolute",
+					top: "152px",
+					left: "50%",
+					transform: "translate(-50%, -50%)",
+					fontFamily: "BIG SHOT",
+					color: "white",
+					fontSize: "10px",
+				}}
+			>
+				{item?.Mana}
+			</div>
+			<div
+				style={{
+					textShadow:
+						"2px 0 0 black, -2px 0 0 black, 0 2px 0 black, 0 -2px 0 black, 1px 1px black, -1px -1px black, 1px -1px black, -1px 1px black",
+					position: "absolute",
+					top: "170px",
+					left: "21px",
+					fontFamily: "BIG SHOT",
+					color: "white",
+					fontSize: "10px",
+				}}
+			>
+				<div
+					style={{
+						flexDirection: "row",
+						display: "flex",
+						alignItems: "center",
+					}}
+				>
+					<img src={SPI} width={18} style={{ marginRight: "4px" }} alt={""} />
+					<span>{item?.SPI}</span>
+				</div>
+			</div>
+			<div
+				style={{
+					textShadow:
+						"2px 0 0 black, -2px 0 0 black, 0 2px 0 black, 0 -2px 0 black, 1px 1px black, -1px -1px black, 1px -1px black, -1px 1px black",
+					position: "absolute",
+					top: "190px",
+					left: "21px",
+					fontFamily: "BIG SHOT",
+					color: "white",
+					fontSize: "10px",
+				}}
+			>
+				<div
+					style={{
+						flexDirection: "row",
+						display: "flex",
+						alignItems: "center",
+					}}
+				>
+					<img src={DEF} width={18} style={{ marginRight: "4px" }} alt={""} />
+					<span>{item?.DEF}</span>
+				</div>
+			</div>
+			<div
+				style={{
+					textShadow:
+						"2px 0 0 black, -2px 0 0 black, 0 2px 0 black, 0 -2px 0 black, 1px 1px black, -1px -1px black, 1px -1px black, -1px 1px black",
+					position: "absolute",
+					top: "170px",
+					left: "100px",
+					fontFamily: "BIG SHOT",
+					color: "white",
+					fontSize: "10px",
+				}}
+			>
+				<div
+					style={{
+						flexDirection: "row",
+						display: "flex",
+						alignItems: "center",
+					}}
+				>
+					<img src={ATK} width={18} style={{ marginRight: "4px" }} alt={""} />
+					<span>{item?.ATK}</span>
+				</div>
+			</div>
+			<div
+				style={{
+					textShadow:
+						"2px 0 0 black, -2px 0 0 black, 0 2px 0 black, 0 -2px 0 black, 1px 1px black, -1px -1px black, 1px -1px black, -1px 1px black",
+					position: "absolute",
+					top: "190px",
+					left: "100px",
+					fontFamily: "BIG SHOT",
+					color: "white",
+					fontSize: "10px",
+				}}
+			>
+				<div
+					style={{
+						flexDirection: "row",
+						display: "flex",
+						alignItems: "center",
+					}}
+				>
+					<img src={SPD} width={18} style={{ marginRight: "4px" }} alt={""} />
+					<span>{item?.SPD}</span>
+				</div>
+			</div>
+		</div>
+	);
+}
--- frontend/src/pages/Assets/index.jsx
+++ frontend/src/pages/Assets/index.jsx
@@ -17,98 +17,130 @@ import card3 from "@assets/card3.png";
 import Button from "@components/Button/index.jsx";
 import { CallData } from "starknet";
 import { useNavigate } from "react-router-dom";
+import CardDetail from "@components/CardDetail/index.jsx";
 
-const style = {background: "#0092ff", padding: "8px 0"};
+const style = { background: "#0092ff", padding: "8px 0" };
 
 export default function Assets() {
-    
-    const [info, setInfo] = useState({})
-    const {account, address, status, chainId, isConnected} = useAccount();
-    const [isModalOpen1, setIsModalOpen1] = useState(false);
-    const navigate = useNavigate()
-    const [addressInfo, setAddressInfo] = useState({})
-    const [hhh, setHhh] = useState("");
-    
-    
-    useEffect(() => {
-        BACKEND.staticInfo().then((result) => {
-            console.log("static info: ", result.data)
-            setInfo(result.data)
-        })
-        if (address) {
-            BACKEND.addressInfo(address).then(result => {
-                console.log("address info: ", result.data)
-                setAddressInfo(result.data)
-            })
-        }
-    }, [address, hhh])
-    
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setHhh(new Date().getTime().toString());
-        }, 5000);
-        
-        return () => clearInterval(interval);
-    }, []);
-    
-    const openChest = async () => {
-        const multiCall = await account.execute([{
-            contractAddress: NEKOMOTO_ADDRESS,
-            entrypoint: "starter_pack",
-            calldata: CallData.compile({})
-        }])
-        // console.log("multiCall: ", multiCall)
-        const result = await account.waitForTransaction(multiCall.transaction_hash)
-        console.log("result: ", result)
-        setIsModalOpen1(true)
-    }
-    
-    return (
-        <div>
-            
-            <NekoModal
-                title="Starter Pack"
-                open={isModalOpen1}
-                onCancel={() => setIsModalOpen1(false)}
-            >
-                <Flex justify="center" vertical="column">
-                    <div className="modal-card">
-                        <div className="modal-card-inner">
-                            <CardCorner/>
-                            <img src={card3} width={192} alt=""/>
-                        </div>
-                    </div>
-                    <Button
-                        text="GO CHECK"
-                        color="yellow"
-                        longness="short"
-                        style={{marginTop: "48px"}}
-                        onClick={() => navigate("/detail2")}
-                    />
-                </Flex>
-            </NekoModal>
-            
-            <div className="assets padding-top-80 padding-bottom-80">
-                <Row gutter={16}>
-                    <Col style={{marginTop: '16px'}} className="gutter-row" xs={24} sm={24} lg={18}>
-                        <InputCard/>
-                    </Col>
-                    <Col style={{marginTop: '16px'}} className="gutter-row" xs={24} sm={24} lg={6}>
-                        <BoxCard title="Starter Pack"
-                                 buttonText={addressInfo.IsStarter === true ? "open" : "already opened"}
-                                 onButtonClick={addressInfo.IsStarter === true ? openChest : null}/>
-                    </Col>
-                </Row>
-                <Row style={{marginTop: '16px', marginBottom: '16px'}}>
-                    <Col xs={24}>
-                        <InfoCard totalRewards={info.totalRewards} treasuryRevenue={info.treasuryRevenue} totalBurn={info.totalBurn}/>
-                    </Col>
-                </Row>
-                
-                <Row>
-                    <Table records={info.treasuryRevenue}/>
-                </Row>
-            </div>
-        </div>
-    );
+	const [info, setInfo] = useState({});
+	const { account, address, status, chainId, isConnected } = useAccount();
+	const [isModalOpen1, setIsModalOpen1] = useState(false);
+	const navigate = useNavigate();
+	const [addressInfo, setAddressInfo] = useState({});
+	const [hhh, setHhh] = useState("");
+
+	useEffect(() => {
+		BACKEND.staticInfo().then((result) => {
+			console.log("static info: ", result.data);
+			setInfo(result.data);
+		});
+		if (address) {
+			BACKEND.addressInfo(address).then((result) => {
+				console.log("address info: ", result.data);
+				setAddressInfo(result.data);
+			});
+		}
+	}, [address, hhh]);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setHhh(new Date().getTime().toString());
+		}, 5000);
+
+		return () => clearInterval(interval);
+	}, []);
+
+	const openChest = async () => {
+		const multiCall = await account.execute([
+			{
+				contractAddress: NEKOMOTO_ADDRESS,
+				entrypoint: "starter_pack",
+				calldata: CallData.compile({}),
+			},
+		]);
+		// console.log("multiCall: ", multiCall)
+		const result = await account.waitForTransaction(multiCall.transaction_hash);
+		console.log("result: ", result);
+		setIsModalOpen1(true);
+	};
+
+	return (
+		<div>
+			<NekoModal
+				title="Starter Pack"
+				open={isModalOpen1}
+				onCancel={() => setIsModalOpen1(false)}
+			>
+				<Flex justify="center" vertical="column">
+					<div className="modal-card">
+						<div className="modal-card-inner">
+							<CardCorner />
+							{/* <img src={card3} width={192} alt=""/> */}
+							<CardDetail
+								item={{
+									Level: 1,
+									SPI: 5,
+									ATK: 3,
+									DEF: 3,
+									SPD: 1,
+									Fade: 125,
+									Mana: 0.234,
+									Element: "Fire",
+								}}
+							/>
+						</div>
+					</div>
+					<Button
+						text="GO CHECK"
+						color="yellow"
+						longness="short"
+						style={{ marginTop: "48px" }}
+						onClick={() => navigate("/detail2")}
+					/>
+				</Flex>
+			</NekoModal>
+
+			<div className="assets padding-top-80 padding-bottom-80">
+				<Row gutter={16}>
+					<Col
+						style={{ marginTop: "16px" }}
+						className="gutter-row"
+						xs={24}
+						sm={24}
+						lg={18}
+					>
+						<InputCard />
+					</Col>
+					<Col
+						style={{ marginTop: "16px" }}
+						className="gutter-row"
+						xs={24}
+						sm={24}
+						lg={6}
+					>
+						<BoxCard
+							title="Starter Pack"
+							buttonText={
+								addressInfo.IsStarter === true ? "open" : "already opened"
+							}
+							onButtonClick={addressInfo.IsStarter === true ? openChest : null}
+						/>
+					</Col>
+				</Row>
+				<Row style={{ marginTop: "16px", marginBottom: "16px" }}>
+					<Col xs={24}>
+						<InfoCard
+							totalRewards={info.totalRewards}
+							treasuryRevenue={info.treasuryRevenue}
+							totalBurn={info.totalBurn}
+						/>
+					</Col>
+				</Row>
+
+				<Row>
+					<Table records={info.treasuryRevenue} />
+				</Row>
+			</div>
+		</div>
+	);
 }
